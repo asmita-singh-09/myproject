d0d3a8d02568f056522ae3ffbd814a3dd3eec26c
asmita-singh-09 <asmita.singh@wissen.com>
2022-01-11 09:20:29 +0530

    updated login.java

--- login.java
+++ login.java
@@ -1,7 +1,12 @@
 public class login{
 
+	public void changepassword(){
+	System.out.println("change password added");
+	}
 	public static void main(String[] args) {
 		System.out.println("login method");
+		System.out.println("resend OTP");
 	}
+	
 
 }
